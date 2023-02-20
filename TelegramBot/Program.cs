3394f799e27919aa193dcf90a8fc12ad9ce1a3f8
Apocrifon <astakhov.stepan2003@gmail.com>
2023-02-20 18:39:59 +0500

    upd

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -4,14 +4,13 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using System.Xml;
-using Microsoft.VisualBasic;
 
 namespace TelegramBot
 {
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("6249665060:AAFNRFlI-pLFSV04Fa2vmqPmcf6vNvhqF1k");
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        static ITelegramBotClient bot = new TelegramBotClient(System.IO.File.ReadAllText("tokens.txt"));
+        public static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
@@ -27,10 +26,8 @@ namespace TelegramBot
                     }
                     if (message.Text != null && message.Text.ToLower() == "расписание")
                     {
-                        await botClient.SendPhotoAsync(message.Chat, photo: "https://ibb.co/CJfL3Dh");
                         await botClient.SendTextMessageAsync(message.Chat, "Расписание на " + DateTime.Now.ToShortDateString());
                     }
-                    await botClient.SendTextMessageAsync(message.Chat, "Короче, лучше замутайте бота, он отвечает, только когда я зафиксил баги:)");
                 }
                 catch
                 {
@@ -39,7 +36,7 @@ namespace TelegramBot
             }
         }
 
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        public static async Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
                 Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
         }
@@ -50,8 +47,8 @@ namespace TelegramBot
             Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
 
                 bot.StartReceiving(
-                    HandleUpdateAsync,
-                    HandleErrorAsync
+                    Update,
+                    Error
                     );
                 Console.ReadLine();
 
